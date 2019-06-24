d63ebcc7316195063a67af6302b8fadb350e4275
AnkeKing <2678924540@qq.com>
2019-06-24 10:00:10 +0800

    return res

--- http/index.js
+++ http/index.js
@@ -1,23 +1,26 @@
 function http(url, data, method) {
-    wx.showToast({
-        title: "加载中...",
-        icon:"loading"
+  wx.showToast({
+    title: "加载中...",
+    icon: "loading"
+  })
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url,
+      data,
+      method: method || 'get',
+      success: function (res) {//成功
+        if (res.statusCode == 200) {
+          resolve(res);
+        }
+
+      },
+      fail: function (err) {//失败
+        reject(err);
+      },
+      complete: function (res) {//完成
+        wx.hideToast();
+      }
     })
-    return new Promise((resolve, reject) => {
-        wx.request({
-            url,
-            data,
-            method: method || 'get',
-            success: function (res) {//成功
-                resolve(res);
-            },
-            fail: function (err) {//失败
-                reject(err);
-            },
-            complete: function (res) {//完成
-                 wx.hideToast();
-            }
-        })
-    })
+  })
 }
-module.exports=http;
+module.exports = http;
